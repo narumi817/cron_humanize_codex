69529ebd5c56b8671abf78de3fa5e6f2d1068f69
narumi817 <s2cartoon817@gmail.com>
2026-05-28 16:11:26 +0900

    未使用のRails生成物を整理する

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,20 +28,8 @@ Rails.application.configure do
   # Change to :null_store to avoid any caching.
   config.cache_store = :memory_store
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  # Make template changes take effect immediately.
-  config.action_mailer.perform_caching = false
-
-  # Set localhost to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "localhost", port: 3001 }
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
-
-  # Highlight code that enqueued background job in logs.
-  config.active_job.verbose_enqueue_logs = true
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
